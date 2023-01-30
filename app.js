8236a74e928261993be4a0b25332323a0e05dde5
Hamdani lmekki <Hamdani.lmekki@gmail.com>
2023-01-30 20:46:45 +0100

    first init
    many js css and html

---
+++ app.js
@@ -0,0 +1,8 @@
+const Work = {daily : 5, pDaily :7, weekly :32, pWeekly:36, monthly:103, pMonthly:128}
+const Play = {daily : 1, pDaily :2, weekly :10, pWeekly:8, monthly:23, pMonthly:29}
+const Study = {daily : 0, pDaily :1, weekly :4, pWeekly:77, monthly:113, pMonthly:19}
+const Exercise = {daily : 1, pDaily :1, weekly :4, pWeekly:5, monthly:11, pMonthly:18}
+const Social = {daily : 1, pDaily :3, weekly :5, pWeekly:10, monthly:21, pMonthly:23}
+const SelfCare = {daily : 0, pDaily :1, weekly :2, pWeekly:2, monthly:7, pMonthly:11}
+
+
